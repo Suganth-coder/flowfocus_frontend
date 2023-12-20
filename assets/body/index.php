2f10498755067cb942751c68a2892b6742ee2fe6
Suganth <sugupr1234@gmail.com>
2023-12-20 10:31:17 +0530

    adding count-up/down timer functionality

--- assets/body/index.php
+++ assets/body/index.php
@@ -1,1 +1,13 @@
+
+<div id="clock_container">
 <div id="container"></div>
+<div id="runner"></div>
+</div>
+
+
+
+<script src="./node_modules/jquery.runner/build/jquery.runner-min.js"></script>
+<button class="start">start</button>
+<button class="continue">continue</button>
+<button class="stop">stop</button>
+<button class="reset">reset</button>
--- js/src/index.js
+++ js/src/index.js
@@ -1,14 +1,44 @@
 $(document).ready(function() {
+
+    $('.continue').hide();
     var ProgressBar = require('progressbar.js');
     var bar = new ProgressBar.Circle("#container", {
-        strokeWidth: 6,
+        strokeWidth: 8,
         easing: 'easeInOut',
-        duration: 1400,
-        color: '#FFEA82',
-        trailColor: '#eee',
-        trailWidth: 1,
+        duration: 3600000,
+        color: 'rgb(83,179,242)',
+        trailColor: '#D5E6FA',
+        trailWidth: 11,
         svgStyle: null
     });
 
-    bar.animate(1.0); // Number from 0.0 to 1.0
+
+    console.log('testing');
+
+    $('#runner').runner();
+
+    $('.start').click(function() {
+        $('#runner').runner('start');
+        bar.animate(1.0);
+        $('.start').hide();
+        $('.continue').show();
+
+    })
+
+    $('.continue').click(function() {
+        $('#runner').runner('start');
+        bar.resume();
+    })
+
+    $('.stop').click(function() {
+        $('#runner').runner('stop');
+        bar.pause();
+    })
+
+    $('.reset').click(function() {
+        $('#runner').runner('reset');
+        bar.set(0.0);
+        $('.start').show();
+        $('.continue').hide();
+    })
 })
